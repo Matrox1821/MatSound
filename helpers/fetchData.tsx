32128f9cf1b2c255a8140137563a111dc72e2847
Galarza Matías <matiasgg1821@gmail.com>
2023-09-29 22:04:38 -0300

    change fetch https

--- helpers/fetchData.tsx
+++ helpers/fetchData.tsx
@@ -1,33 +1,37 @@
 export const getUserPlaylists = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/user/${url}`).then((r) => r.json());
+  fetch(`https://matfy-eight.vercel.app/api/user/${url}`).then((r) => r.json());
 
 export const getCurrentUserPlaylists = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/user/playlists${url}`).then((r) =>
+  fetch(`https://matfy-eight.vercel.app/api/user/playlists${url}`).then((r) =>
     r.json()
   );
 
 export const getArtistsFollowing = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/user/following?${url}`).then((r) =>
+  fetch(`https://matfy-eight.vercel.app/api/user/following?${url}`).then((r) =>
     r.json()
   );
 
 export const getPlaylistById = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/playlist?${url}`).then((r) =>
+  fetch(`https://matfy-eight.vercel.app/api/playlist?${url}`).then((r) =>
     r.json()
   );
 
 export const getAlbumById = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/album?${url}`).then((r) => r.json());
+  fetch(`https://matfy-eight.vercel.app/api/album?${url}`).then((r) =>
+    r.json()
+  );
 
 export const getArtistAlbums = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/artist/albums?${url}`).then((r) =>
+  fetch(`https://matfy-eight.vercel.app/api/artist/albums?${url}`).then((r) =>
     r.json()
   );
 
 export const getTraksById = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/track?${url}`).then((r) => r.json());
+  fetch(`https://matfy-eight.vercel.app/api/track?${url}`).then((r) =>
+    r.json()
+  );
 
 export const getArtistById = (url: string) =>
-  fetch(`http://matfy-eight.vercel.app/api/artist?${url}`).then((r) =>
+  fetch(`https://matfy-eight.vercel.app/api/artist?${url}`).then((r) =>
     r.json()
   );
