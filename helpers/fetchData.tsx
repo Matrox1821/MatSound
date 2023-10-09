b9614d20daf29b9669881a49a04931fd78b57579
Galarza Matías <matiasgg1821@gmail.com>
2023-10-09 16:10:12 -0300

    fix error

--- helpers/fetchData.tsx
+++ helpers/fetchData.tsx
@@ -27,33 +27,31 @@ export const getTrakById = (url: string) =>
 
 //user
 export const getUserPlaylists = (url: string) =>
-  fetch(`https://mat-sound.vercel.app//api/user/${url}`).then((r) => r.json());
+  fetch(`https://mat-sound.vercel.app/api/user/${url}`).then((r) => r.json());
 
 export const getCurrentUserPlaylists = (url: string) =>
-  fetch(`https://mat-sound.vercel.app//api/user/playlists${url}`).then((r) =>
+  fetch(`https://mat-sound.vercel.app/api/user/playlists${url}`).then((r) =>
     r.json()
   );
 
 export const getArtistsFollowing = (url: string) =>
-  fetch(`https://mat-sound.vercel.app//api/user/following?${url}`).then((r) =>
+  fetch(`https://mat-sound.vercel.app/api/user/following?${url}`).then((r) =>
     r.json()
   );
 
 //playlist
 export const getPlaylistById = (url: string) =>
-  fetch(`https://mat-sound.vercel.app//api/playlist?${url}`).then((r) =>
+  fetch(`https://mat-sound.vercel.app/api/playlist?${url}`).then((r) =>
     r.json()
   );
 
 //album
 export const getAlbumById = (url: string) =>
-  fetch(`https://mat-sound.vercel.app//api/album?${url}`).then((r) => r.json());
+  fetch(`https://mat-sound.vercel.app/api/album?${url}`).then((r) => r.json());
 //artist
 export const getArtistById = (url: string) =>
-  fetch(`https://mat-sound.vercel.app//api/artist/${url}`).then((r) =>
-    r.json()
-  );
+  fetch(`https://mat-sound.vercel.app/api/artist/${url}`).then((r) => r.json());
 
 //track
 export const getTrakById = (url: string) =>
-  fetch(`https://mat-sound.vercel.app//api/track?${url}`).then((r) => r.json());
+  fetch(`https://mat-sound.vercel.app/api/track?${url}`).then((r) => r.json());
