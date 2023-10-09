085878d2817ba9a4686d200c87f0a2dc3fbd58b3
Galarza Matías <matiasgg1821@gmail.com>
2023-10-09 19:22:42 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import {Playlists} from "./Playlists";
 export default function Home() {
   return (
     <article className="p-4 pt-[4rem] h-full" id="container_body">
-      <Artists />
-      <Playlists />
+      {/* <Artists />
+      <Playlists /> */}
     </article>
   );
 }
