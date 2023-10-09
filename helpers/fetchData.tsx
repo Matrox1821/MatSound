3b0aa90f6f854ff4ced28e18099ee53d0e3b9723
Galarza Matías <matiasgg1821@gmail.com>
2023-10-09 16:07:12 -0300

    fetch data changes

--- helpers/fetchData.tsx
+++ helpers/fetchData.tsx
@@ -1,4 +1,4 @@
-//user
+/* //user
 export const getUserPlaylists = (url: string) =>
   fetch(`http://localhost:3000/api/user/${url}`).then((r) => r.json());
 
@@ -23,43 +23,37 @@ export const getArtistById = (url: string) =>
 
 //track
 export const getTrakById = (url: string) =>
-  fetch(`http://localhost:3000/api/track?${url}`).then((r) => r.json());
+  fetch(`http://localhost:3000/api/track?${url}`).then((r) => r.json()); */
 
-/* export const getUserPlaylists = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/user/${url}`).then((r) => r.json());
+//user
+export const getUserPlaylists = (url: string) =>
+  fetch(`https://mat-sound.vercel.app//api/user/${url}`).then((r) => r.json());
 
 export const getCurrentUserPlaylists = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/user/playlists${url}`).then((r) =>
+  fetch(`https://mat-sound.vercel.app//api/user/playlists${url}`).then((r) =>
     r.json()
   );
 
 export const getArtistsFollowing = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/user/following?${url}`).then((r) =>
+  fetch(`https://mat-sound.vercel.app//api/user/following?${url}`).then((r) =>
     r.json()
   );
 
+//playlist
 export const getPlaylistById = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/playlist?${url}`).then((r) =>
+  fetch(`https://mat-sound.vercel.app//api/playlist?${url}`).then((r) =>
     r.json()
   );
 
+//album
 export const getAlbumById = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/album?${url}`).then((r) =>
-    r.json()
-  );
-
-export const getArtistAlbums = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/artist/albums?${url}`).then((r) =>
-    r.json()
-  );
-
-export const getTraksById = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/track?${url}`).then((r) =>
-    r.json()
-  );
-
+  fetch(`https://mat-sound.vercel.app//api/album?${url}`).then((r) => r.json());
+//artist
 export const getArtistById = (url: string) =>
-  fetch(`https://matfy-eight.vercel.app/api/artist?${url}`).then((r) =>
+  fetch(`https://mat-sound.vercel.app//api/artist/${url}`).then((r) =>
     r.json()
   );
- */
+
+//track
+export const getTrakById = (url: string) =>
+  fetch(`https://mat-sound.vercel.app//api/track?${url}`).then((r) => r.json());
