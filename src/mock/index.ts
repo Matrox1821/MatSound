623784841d6a8074e7af9b21c90f9a43605bcabc
Matrox1821 <matiasgg1821@gmail.com>
2024-09-24 03:26:57 -0300

    add image extractor

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,11 +1,17 @@
 import undeadMp3 from "./../assets/UNDEAD.mp3";
 import undeadJpg from "./../assets/undead-yoasobi.jpg";
+
+import { average } from "color.js";
+const background = document.getElementById("background-color");
+const image = document.getElementById("image");
+
 const TracksWithoutId = [
   { song: undeadMp3, image: undeadJpg, name: "Undead", artist: "YOASOBI" },
 ];
 
 export const TRACKS = TracksWithoutId.map((track) => {
-  const { name, artist, ...rest } = track;
+  const { name, artist, image, ...rest } = track;
   const id = `${name}-${artist}`;
-  return { ...rest, name, artist, id };
+  console.log(average(image.src, { format: "hex" }));
+  return { ...rest, name, artist, id, image };
 });
